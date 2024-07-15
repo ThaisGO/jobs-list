274dd884185d9aa84e10dc994cf821762e27ff06
ThaisGO <thaisgo.viana@gmail.com>
2024-07-15 15:07:40 -0300

    fix: axios call

--- src/composables/getJob.js
+++ src/composables/getJob.js
@@ -7,17 +7,15 @@ export default function useJob(id) {
 
     const getJob = async () => {
         try {
-            axios
-                .get(`https://json-serve-list.vercel.app/jobs/${id}`)
-                .then((response) => {
-                    if (response.status === 200) {
-                        job.value = response.data;
-                    }  else if (response.status === 404) {
-                        throw new Error('The requested resource does not exist or has been deleted')
-                    } else {
-                        throw new Error('An unexpected error occurred')
-                    }
-                });
+            const response = await axios.get(`https://json-serve-list.vercel.app/jobs/${id}`);
+            
+            if (response.status === 200) {
+                job.value = response.data;
+            } else if (response.status === 404) {
+                throw new Error('The requested resource does not exist or has been deleted');
+            } else {
+                throw new Error('An unexpected error occurred');
+            }
         } catch (err) {
             error.value = err.message
             console.error('Error fetching jobs:', err.message)
